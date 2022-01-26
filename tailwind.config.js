29a30a2276e72fbab0b4427a3bbec367cc02826f
wilmion <wilmion92@gmail.com>
2022-01-26 17:13:56 -0500

    Adding tailwin config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  mode: "jit",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
